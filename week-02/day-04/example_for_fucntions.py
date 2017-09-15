0c73a965f46c868e13fbea0bfb9d01b066b884ef
Attila Korom <attilakorom2014@gmail.com>
2017-09-15 17:39:18 +0200

    fix(example_for_functions)

--- week-02/day-04/example_for_fucntions.py
+++ week-02/day-04/example_for_fucntions.py
@@ -4,28 +4,41 @@ import random
 # Create function for read the parameter
 # Create function for randomed numbers
 # Check the numbers
+print("You have 5 lifes!")
 
-def read_user_parameters():
-    user_number = int(input("Your number(0-10): "))
+def read_user_parameters(): 
+    user_number = int(input("Your number(1-100): "))
     return user_number
 
 def create_random_numbers():
     random_number = 0
-    random_number = random.randint(0,10)
+    random_number = random.randint(1,100)
     return random_number
+
 
 def check_numbers(read_user_parameters, create_random_numbers):
     if read_user_parameters == create_random_numbers:
-        return True
+        return 0
+    elif read_user_parameters > create_random_numbers:
+        return 1
     else:
-        return False
+        return -1
 
 
 def user_message(check_numbers):
-    if check_numbers == False:
-        print("This is not the number!!")
-        print("Number was "+ str(create_random_numbers()))
-    else:
-        print("Congratulation! This is the number!!")
+    random_numb = create_random_numbers
+    life_number = 5
+    for i in range(4):
+        if life_number != 0:
+            if check_numbers(read_user_parameters(), random_numb) == 1:
+                print("My number is lower!")
+            elif check_numbers(read_user_parameters(), random_numb) == 0:
+                print("Congratulation!You found the number!")
+                break
+            else:
+                print("My number is bigger!)
+        else:
+            print("GAME OVER DUDE!")
 
+            
 user_message(check_numbers(read_user_parameters(), create_random_numbers()))
