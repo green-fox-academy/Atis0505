4ca17bc9289c82a5d51690473aa8c2460b05d4fe
Attila Korom <attilakorom2014@gmail.com>
2017-09-17 21:54:45 +0200

    feat(count_lines): create function

---
+++ week-03/day-01/count_lines.py
@@ -0,0 +1,20 @@
+# Write a function that takes a filename as string,
+# then returns the number of lines the file contains.
+# It should return zero if it can't open the file, and
+# should not raise any error.
+
+file_address = "example_file_02.txt"
+
+def read_file(file_txt):
+    try:
+        counter = 0
+        file = open(file_txt,"r")
+        my_file = file.readlines()
+        for line in my_file:
+            counter +=1
+        file.close()
+        print("Lines number in the file: ",counter)
+    except IOError:
+        return 0
+
+print(read_file(file_address))
