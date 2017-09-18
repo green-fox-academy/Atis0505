9570ba1c0cb10cf5e60abfb6d8b57b25192f35eb
Attila Korom <attilakorom2014@gmail.com>
2017-09-18 12:05:31 +0200

    feat(reverse_lines.py): complete

--- week-03/day-01/reverse_lines.py
+++ week-03/day-01/reverse_lines.py
@@ -1,3 +1,18 @@
 # Create a method that decrypts reversed-lines.txt
-def decrypt(file_name):
-    pass
+def decrypt(file_text):
+    try:
+        f = open(file_text, "r")
+        file_string = f.readlines()
+        line_text = ""
+        to_file = open("normal_text_from_reversed_lines.txt", "a")
+        for line in file_string:
+            for i in range((len(line)-1),0,-1):
+                line_text += line[i]
+        to_file.write(line_text)
+        f.close()
+        to_file.close()
+        print("Done")
+    except IOError:
+        print("Unable to read file, ", file_text)
+
+decrypt("reversed_lines.txt")
