fc12cd54cb5c3d27128d0529e656d6d1ed547aa9
Attila Korom <attilakorom2014@gmail.com>
2017-09-20 11:57:05 +0200

    feat(position_square): complete drawinf function

---
+++ week-03/day-03/position_square.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+x = 0
+y = 0
+
+# create a square drawing function that takes 2 parameters:
+# the x and y coordinates of the square's top left corner
+# and draws a 50x50 square from that point.
+# draw 3 squares with that function.
+def sqaure_drawing(xparam, yparam):
+    moving_size = 0;
+    for i in range(3):
+        teal_line = canvas.create_rectangle(0+moving_size,0+moving_size,50+moving_size,50+moving_size, fill="orange")
+        moving_size += 50
+
+
+sqaure_drawing(int(x),int(y))
+root.mainloop()
