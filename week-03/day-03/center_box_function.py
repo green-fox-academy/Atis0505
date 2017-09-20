78b131e1d502f5d284832270dc851e7618875719
Attila Korom <attilakorom2014@gmail.com>
2017-09-20 12:07:05 +0200

    feat(center_box_function.py): completed draw function

---
+++ week-03/day-03/center_box_function.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+
+window_width = 300
+window_height = 300
+
+# create a square drawing function that takes 1 parameter:
+# the square size
+# and draws a square of that size to the center of the canvas.
+# draw 3 squares with that function.
+
+canvas = Canvas(root, width = window_width, height = window_height)
+canvas.pack()
+
+size_number = int(input("Size of the square: "))
+
+def draw_square_in_middle(size_value):
+    lime_box = canvas.create_rectangle(window_width/2-size_number/2, window_height/2-size_number/2, window_width/2+size_number/2, window_height/2+size_number/2, fill='lime green')
+
+
+draw_square_in_middle(size_number)
+root.mainloop()
