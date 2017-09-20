d8de557ffb4293a175a5984e4c5c023aff6ed88f
Attila Korom <attilakorom2014@gmail.com>
2017-09-20 16:17:43 +0200

    feat(line_play.py): created for ciklus for two drawing method

---
+++ week-03/day-03/line_play.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# reproduce this:
+# [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
+
+def lines_drawer():     
+    for i in range(10,291,10):
+        teal_line = canvas.create_line(i, 0, 300, i, fill='red')
+        teal_line = canvas.create_line(0, i, i, 300, fill='green')
+
+lines_drawer()
+
+root.mainloop()
+
+
