581c505a0d7812dd6c32e8a2cc8bdb6663396ee5
Attila Korom <attilakorom2014@gmail.com>
2017-09-25 13:48:34 +0200

    feat(counter.py): check with unit test file

---
+++ week-04/day-01/Counter/counter.py
@@ -0,0 +1,16 @@
+class Counter(object):
+    def __init__(self, number = 0):
+        self.count_number = number
+
+    def add(self, number = None):
+        if number != None:
+            self.count_number += number
+        else:
+            self.count_number +=1
+
+    def get(self):
+        return self.count_number
+    
+    def reset(self):
+        self.__init__()
+
