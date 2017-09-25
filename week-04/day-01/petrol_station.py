ea444de5b21abe8f988ff13eb08307ead15fc408
Attila Korom <attilakorom2014@gmail.com>
2017-09-25 14:39:55 +0200

    feat(station.py): create refill function

---
+++ week-04/day-01/petrol_station.py
@@ -0,0 +1,18 @@
+class Station(object):
+    def __init__(self):
+        self.gasAmount = 1500
+
+    def refill(self):
+        self.gasAmount -= Car.capacity
+
+
+class Car(object):
+    gas_amount = 0
+    capacity = 100
+        
+
+station_01 = Station()
+car_01 = Car()
+
+station_01.refill()
+print(station_01.gasAmount)
