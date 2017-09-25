a112e977643f491b64173af00c8a9d64562e35fe
Attila Korom <attilakorom2014@gmail.com>
2017-09-25 11:53:27 +0200

    feat(domino.py):created double loop for dominos swap

--- week-04/day-01/domino.py
+++ week-04/day-01/domino.py
@@ -1,2 +1,7 @@
-list_of_number = [2, 4], [4, 3], [3, 5]
+class Domino(object):
+    def __init__(self, value_a, value_b):
+        self.values = [value_a, value_b]
+
+    def __repr__(self):
+        return '[{}, {}]'.format(self.values[0], self.values[1])
 
