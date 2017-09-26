98a9285c94f609cb8b3ef00e359c7532c053a2e6
Attila Korom <attilakorom2014@gmail.com>
2017-09-26 14:30:43 +0200

    feat(flower.py): skeleton of flower class

---
+++ week-04/day-02/Garden_project/Flower.py
@@ -0,0 +1,13 @@
+class Flower(object):
+    def __init__(self, color = "green", amount = 50):
+        self.color = color
+        self.amount = amount
+
+    def watering(self):
+        self.amount += 10
+    
+    def info(self):
+        if self.amount > 65:
+            print("The {} color flower doesn't need water.".format(self.color))
+        else:
+            print("The {} color flower needs water.".format(self.color))
