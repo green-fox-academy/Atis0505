719f0c31b9ab5047d211ed91c4625b0073a53146
Attila Korom <attilakorom2014@gmail.com>
2017-09-26 11:54:44 +0200

    feat(PallidaClass.py): create skeleton of pallidaclass

---
+++ week-04/day-02/GreenFox_org/PallidaClass.py
@@ -0,0 +1,20 @@
+from Student import Student
+from Mentor import Mentor
+
+class PallidaClass:
+    def __init__(self, class_name):
+        self.class_name = class_name
+        self.list_of_students = []
+        self.list_of_mentors = []
+
+
+    def add_student(self, student):
+        self.list_of_students.append(student)
+
+
+    def add_mentor(self, mentor):
+        self.list_of_mentors.append(mentor)
+
+
+    def info():
+        pass
