c50cac717152e02b751c60c6f0048c579889a3ce
Attila Korom <attilakorom2014@gmail.com>
2017-09-26 11:06:40 +0200

    feat(Person.py): create person class

---
+++ week-04/day-02/GreenFox_org/Person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self. gender = gender
+
+    
+    def introduce():
+        print('Hi, I\'m {}, a {} years old {}'.format(self.name, self.age, self.gender))
+
+    
+    def get_goal():
+        print("My goal is: Live for the moment!")
