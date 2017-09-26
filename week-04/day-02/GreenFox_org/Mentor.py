35dbcafbfd86455995b8f6faf2ae76e993ed195d
Attila Korom <attilakorom2014@gmail.com>
2017-09-26 11:38:19 +0200

    feat(Mentor.py): create mentor class

---
+++ week-04/day-02/GreenFox_org/Mentor.py
@@ -0,0 +1,13 @@
+from Person import Person
+
+class Mentor(Person):
+    def __init__(self, level):
+        self.level = level
+
+    
+    def get_goal(self):
+        print("Educate brilliant junior software developers.")
+
+
+    def introduce(self):
+        print("Hi, I'm {}, a {} year old {} {} mentor.".format(self.name, self.age, self.gender, self.level))
