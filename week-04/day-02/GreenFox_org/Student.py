4259d575e21cddf6afa51a70e5451b48b26fb159
Attila Korom <attilakorom2014@gmail.com>
2017-09-26 11:22:54 +0200

    feat(student.py): create student class

---
+++ week-04/day-02/GreenFox_org/Student.py
@@ -0,0 +1,18 @@
+from Person import Person
+
+class Student(Person):
+    def __init__(self, previous_organization, skipped_days):
+        super().__init__()
+        self.previous_organization = previous_organization
+        self.skipped_days = skipped_days
+
+    def get_goal(self):
+         print("Be a junior software developer.")
+
+
+    def introduce(self):
+        print('Hi, I\'m {}, a {} year old {} from {} who skipped {} days from the course already.'.format(self.name,self.age, self.gender, self.previous_organization, self. skipped_days))
+
+
+    def skip_days(self, number_of_days = 0):
+        skip_days += number_of_days
