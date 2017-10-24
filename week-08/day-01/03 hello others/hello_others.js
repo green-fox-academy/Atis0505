496dcfc0c0c5d3e3779dae7d9eb2c571afd9c76d
Attila Korom <attilakorom2014@gmail.com>
2017-10-24 15:59:36 +0200

    feat(hello other): save changes

---
+++ week-08/day-01/03 hello others/hello_others.js
@@ -0,0 +1,10 @@
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log("Hello Zsuzsi!");
+console.log("Hello Peti!");
+console.log("Hello Attila!");
