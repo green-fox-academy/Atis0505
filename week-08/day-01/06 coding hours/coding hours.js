85548c7f9c345e211816b388bbe4cea4e6c00401
Attila Korom <attilakorom2014@gmail.com>
2017-10-24 10:45:02 +0200

    feat(coding hours): print results with strings

---
+++ week-08/day-01/06 coding hours/coding hours.js
@@ -0,0 +1,17 @@
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+var dailyHours = 6;
+var weekNumber = 17;
+
+var codeHours = dailyHours * 5 * weekNumber;
+
+console.log("Coding hours per week: " + codeHours);
+ var precentageAverage = 52 / (7 * 24);
+
+console.log('Precentge hours of the week: ' + precentageAverage);
