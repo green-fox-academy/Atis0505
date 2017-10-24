c5de2e90d4c908c72f32e8c98bb2b00460258725
Attila Korom <attilakorom2014@gmail.com>
2017-10-24 11:45:25 +0200

    feat(basic_info): show variables on console

---
+++ week-08/day-01/10_define_dasic_info/basic_info.js
@@ -0,0 +1,15 @@
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Attila Korom';
+let age = 28;
+let height = 1.78;
+let married = true;
+
+console.log('Name: '+ name + '\n' +
+            "Age: "+ age + '\n' +
+            "Height: " + height + '\n'+
+            "Married: " + married );
