513ec814c0cd86a1d5e9156d3082cfa2d9408284
Attila Korom <attilakorom2014@gmail.com>
2017-10-24 09:53:59 +0200

    feat(two numbers): create all results

---
+++ week-08/day-01/05 two numbers/two numbers.js
@@ -0,0 +1,13 @@
+// Create a program that prints a few operations on two numbers: 22 and 13
+var numb01 = 22;
+var numb02 = 13;
+// Print the result of 13 added to 22
+console.log(numb01+numb02)
+// Print the result of 13 substracted from 22
+console.log(numb01-numb02)
+// Print the result of 22 mutiplied by 13
+console.log(numb01*numb02)
+// Print the result of 22 divided by 13 (as a decimal fraction)
+console.log(numb01%numb02)
+// Print the reminder of 22 divided by 13
+console.log(numb01/numb02)
