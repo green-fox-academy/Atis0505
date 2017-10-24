4a8546a92516ed0ea931e51e77988139fa796f99
Attila Korom <attilakorom2014@gmail.com>
2017-10-24 15:49:31 +0200

    feat(draw_diagonal): add the extra condition

--- week-08/day-01/32_draw_diagonal/draw_diagonal.js
+++ week-08/day-01/32_draw_diagonal/draw_diagonal.js
@@ -11,10 +11,11 @@
 //
 // The square should have as many lines as lineCount is
 
+var lineCount = 6;
 var line ="";
 for(i=0; i<=lineCount; i++){
     for(j=0; j<=lineCount; j++){
-        if(j===0 || i===0 || j===lineCount || i===lineCount){
+        if(j===0 || i===0 || j===lineCount || i===lineCount || i===j){
             line += "%";
         }else{
             line += " ";
