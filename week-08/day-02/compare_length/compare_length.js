0b8679d40f2c027f2a270ce628b63fe1766b2ac3
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 09:28:54 +0200

    feat(compare_length): shorter solution

---
+++ week-08/day-02/compare_length/compare_length.js
@@ -0,0 +1,11 @@
+'use strict';
+// - Create a variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create a variable named `p2`
+//   with the following content: `[4, 5]`
+// - Log to the console if `p2` has more elements than `p1`
+
+var p1 = [1,2,3];
+var p2 = [4,5];
+
+(p1.size<p2.size)?console.log(p2):console.log(p1)
