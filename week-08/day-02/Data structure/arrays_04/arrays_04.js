92a6892e43984ea41bb72d84d79ceb655fabb45a
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 14:35:15 +0200

    feat(arrays_04): change elements

---
+++ week-08/day-02/Data structure/arrays_04/arrays_04.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var shop_items = ["Cupcake", 2, "Brownie", false]
+
+// Accidentally we added "2" and "false" to the array. 
+// Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+// No, don't just remove the items :)
+shop_items[1] = "Croissant";
+shop_items[3] = "Ice cream";
+console.log(shop_items)
