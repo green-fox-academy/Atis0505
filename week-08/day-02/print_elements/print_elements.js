33c8d644cc5e1ecdf9066bba8e9c52f1b0f1f755
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 08:45:43 +0200

    feat(print_elements): use arguments list

--- week-08/day-02/print_elements/print_elements.js
+++ week-08/day-02/print_elements/print_elements.js
@@ -4,8 +4,11 @@
 //   (can have multiple number of arguments)
 
 function printer(input_parameters){
-    console.log(arguments)
-    console.log("Well hi there,",input_parameters)
+    var param_string = ""
+    for(var i=0; i<=arguments.length-1;i++){
+        param_string += arguments[i] + " "
+    }
+    console.log("Well hi there,",param_string)
 }
 
-printer("Attila")
+printer("Attila","Peti","Zsuzsi","Laci")
