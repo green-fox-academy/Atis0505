d726a8259ae9e3a314067db72a2e63470d77358e
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 14:38:18 +0200

    feat(arrays_04): change with different solution

--- week-08/day-02/Data structure/arrays_04/arrays_04.js
+++ week-08/day-02/Data structure/arrays_04/arrays_04.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var shop_items = ["Cupcake", 2, "Brownie", false]
+var shop_items02 = ["Cupcake", 2, "Brownie", false]
 
 // Accidentally we added "2" and "false" to the array. 
 // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
@@ -8,3 +9,6 @@ var shop_items = ["Cupcake", 2, "Brownie", false]
 shop_items[1] = "Croissant";
 shop_items[3] = "Ice cream";
 console.log(shop_items)
+shop_items02[shop_items02.indexOf(2)] = "Croissant";
+shop_items02[shop_items02.indexOf(false)] = "Ice cream";
+console.log(shop_items02)
