3236e6e7a981eb67aed32550fc5d75fe86ff6710
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 08:15:26 +0200

    feat(greeter_function): create function

---
+++ week-08/day-02/greeter_function/greeter_function.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings, dear Greenfox`
+//     - Prepare for the special case when no parameters are given
+// - Greet `al`
+
+var al = "Greenfox"
+
+function greet(input_string){
+    console.log("Greeting, dear",input_string)
+}
+
+greet(al)
