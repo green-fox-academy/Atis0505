5b52fd032bf11945938cfce89b43aef77076def3
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 14:42:25 +0200

    feat(arrays_05): check list contains the number

---
+++ week-08/day-02/Data structure/arrays_05/arrays_05.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Check if the array contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+var numbers = [1,2,3,4,5,6,8];
+
+var result = (numbers.indexOf(7)>-1)?"Hoorray":"Noooooo";
+console.log(result)
