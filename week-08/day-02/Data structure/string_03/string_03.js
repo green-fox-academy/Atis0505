30386ccc2d29a0ee37ba3eb9bb388a2243db4dab
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 13:48:52 +0200

    feat(string_03): replace url

---
+++ week-08/day-02/Data structure/string_03/string_03.js
@@ -0,0 +1,8 @@
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+var url = "https//www.reddit.com/r/nevertellmethebots";
+var right_url = url.replace("bots","odds");
+url = right_url;
+console.log(url);
