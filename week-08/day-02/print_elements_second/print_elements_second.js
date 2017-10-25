fab4e4f3fc9ba4f68963532f780f33f69780f270
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 10:06:48 +0200

    feat(print_elements_second): use var for i variable in for loop

---
+++ week-08/day-02/print_elements_second/print_elements_second.js
@@ -0,0 +1,10 @@
+'use strict';
+// - Create a variable named `af` with the following content: `[4, 5, 6, 7]`
+// - Log each the element of `af` to the console*
+// *hint: use a loop, console.log(af) won't cut it
+// - bonus for using the correct built in array method
+
+var af = [4, 5, 6, 7];
+for(var i=0;i<=af.length-1;i++){
+    console.log(af[i])
+}
