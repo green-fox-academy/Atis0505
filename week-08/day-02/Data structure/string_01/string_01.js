0459dc2bc552f2006aae65e4afede6685abde8b5
Attila Korom <attilakorom2014@gmail.com>
2017-10-25 13:38:27 +0200

    feat(string_01): replace string

---
+++ week-08/day-02/Data structure/string_01/string_01.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var example = "In a dishwasher far far away";
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+var rep = example.replace("dishwasher","galaxy");
+
+console.log(rep)
