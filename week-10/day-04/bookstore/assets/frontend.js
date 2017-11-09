7fe6eae80f2d7ba98ddbff4b80d7f0fe276c0df7
Attila Korom <attilakorom2014@gmail.com>
2017-11-09 19:48:40 +0100

    feat(bookstore_endpoint): create structure of serverconnections

---
+++ week-10/day-04/bookstore/assets/frontend.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let url = 'http://localhost:4000';
+let bodyElement = document.querySelector('body');
+let connectionObject = new XMLHttpRequest();
+
+function talkToApi(method, resource){
+    connectionObject.open(method, url+resource, true);
+    connectionObject.onload = function(){
+        let div = document.createElement('div');
+        div.innerHTML = connectionObject.response;
+        bodyElement.appendChild(div);
+    }
+    connectionObject.send();
+}
+
+talkToApi("GET", "/list");
+// talkToApi("GET", "/all");
---
+++ week-10/day-04/bookstore/bookstore.js
@@ -0,0 +1,66 @@
+'use strict'
+let password_string = 'Alga19890505';
+const mysql = require('mysql');
+let express = require('express');
+let app = express();
+let sql_query_book_title = "SELECT book_name FROM book_mast;";
+express.json.type = "application/json";
+
+app.use(express.json);
+// app.use('/assets',express.static('./assets'));
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: password_string,
+    database: 'bookstore'
+});
+
+app.get('/', function(request, response){
+    console.log("knknkn");
+    response.sendFile(__dirname + '/index.html');
+});
+
+connection.connect(function(err){
+    if(err){
+      console.log(err, "Error connecting to Db");
+      return;
+    }
+    console.log("Connection established");
+});
+
+
+
+app.get('/list', function(request, response) {
+    console.log("ksd ksdakja");
+    connection.query(sql_query_book_title, function(err, rows){
+        if(err){
+            console.log(err.toString());
+        }
+        console.log("Data received from Db:\n");
+        let htmlString = '<ul>';
+        rows.forEach(function(row) {
+            htmlString = htmlString + '<li>' + row.book_name + '<li>';
+        });
+        htmlString = htmlString + '<ul>';
+    })
+    response.send(htmlString);
+});
+
+
+// app.get('/all', function(request, response){
+//     connection.query(sql_query_book_title, function(err, rows){
+//         if(err){
+//             console.log(err.toString());
+//         }
+//         console.log("Data received from Db:\n");
+//         let htmlString = '<table><ul>';
+//         rows.forEach(function(row) {
+//             htmlString = htmlString + '<li>' + row.book_name + '<li>';
+//         });
+//         htmlString = htmlString + '<ul>';
+//     })
+//     response.send(htmlString);
+// });
+
+app.listen(4000);
