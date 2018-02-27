7715d952e3f08f04d39a9748d240d00437286bb1
Atis0505 <attilakorom2014@gmail.com>
2018-02-27 18:36:45 +0100

    feat(greet): create greeting method

---
+++ Csharp-course/week-02/day-01/greeter/greeter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            string al = "Greenfox";
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+            greeting(al);
+            Console.ReadKey();
+        }
+
+        public static void greeting (string name)
+        {
+            Console.WriteLine("Greetings dear, {0}", name);
+        }
+    }
+}
