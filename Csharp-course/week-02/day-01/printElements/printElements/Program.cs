5778869d3d225569a95d9196e77d830c40f59942
Atis0505 <attilakorom2014@gmail.com>
2018-02-28 22:00:12 +0100

    feat(printElements): create for loop for printing all elements

---
+++ Csharp-course/week-02/day-01/printElements/printElements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            int[] af = { 4, 5, 6, 7 };
+            // - Print all the elements of `af`
+            for (int i = 0; i < af.Length; i++)
+            {
+                Console.WriteLine("{0}. element is {1}", i+1, af[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
