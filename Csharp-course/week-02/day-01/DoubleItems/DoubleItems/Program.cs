8597c6a46eb7702942bdaac13ece4e73987c9bf7
Atis0505 <attilakorom2014@gmail.com>
2018-02-28 22:12:30 +0100

    feat(double): multiply all items and print them

---
+++ Csharp-course/week-02/day-01/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            int[] ag = { 3, 4, 5, 6, 7 };
+            // - Double all the values in the array
+            for (int i = 0; i < ag.Length; i++)
+            {
+                ag[i] = ag[i] * 2;
+                Console.WriteLine(ag[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
