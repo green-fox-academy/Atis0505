c33e66751005dd3ef1ba716caecd535a335c6e2b
Atis0505 <attilakorom2014@gmail.com>
2018-02-28 22:52:03 +0100

    feat(reverse): reverse the list and print it

---
+++ Csharp-course/week-02/day-01/reverseList/reverseList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            int[] aj = { 3, 4, 5, 6, 7 };
+            for (int i = 0; i <= aj.Length/2; i++)
+            {
+                int temp = aj[i];
+                aj[i] = aj[aj.Length -1 - i];
+                aj[aj.Length-1 - i] = temp;
+            }
+
+            for (int i = 0; i < aj.Length; i++)
+            {
+                Console.Write(aj[i]+ " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
