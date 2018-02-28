0ac89dbd966b42bca6b111e3ff450e6c19dee8b4
Atis0505 <attilakorom2014@gmail.com>
2018-02-28 22:27:31 +0100

    feat(appendAtoWords): create lopp for appending A

---
+++ Csharp-course/week-02/day-01/AppendAtoWords/AppendAtoWords/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendAtoWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            string[] nimals = { "kuty", "macsk", "cic" };
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                nimals[i] = nimals[i] + "a";
+                Console.WriteLine(nimals[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
