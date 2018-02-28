673a45e52c4631e4c325a1e7b7f7bf48b2a64b34
Atis0505 <attilakorom2014@gmail.com>
2018-02-28 21:49:27 +0100

    feat(sumElements): print elements sum

---
+++ Csharp-course/week-02/day-01/sumElements/sumElements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `r`
+            //   with the following content: `[54, 23, 66, 12]`
+            int[] r = { 54, 23, 66, 12 };
+            Console.WriteLine("Sum of the second and third element: {0}",r[1]+r[2]);
+            // - Print the sum of the second and the third element
+            Console.ReadLine();
+        }
+    }
+}
