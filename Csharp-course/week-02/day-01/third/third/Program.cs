5fcb873da0e685c5665e2f9675105aa26cf3a157
Atis0505 <attilakorom2014@gmail.com>
2018-02-28 20:30:35 +0100

    feat(third): find 3. element from array

---
+++ Csharp-course/week-02/day-01/third/third/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `q`
+             int[]q = new int[] { 4, 5, 6, 7 };
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print the third element of `q`
+            Console.WriteLine("Original array is:");
+            for (int i = 0; i < q.Length; i++)
+            {
+                Console.WriteLine("{0}. item is {1}",i+1,q[i]);
+            }
+            Console.WriteLine("Third element of array is {0}",q[2]);
+            Console.ReadLine();
+        }
+    }
+}
