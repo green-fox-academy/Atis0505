19e97c6e95e62fe5a31c68d6d726a4c14597ad83
Atis0505 <attilakorom2014@gmail.com>
2018-03-01 14:41:26 +0100

    feat(urlFixer): fix the url with replace method

---
+++ Csharp-course/week-02/day-02/simpleReplace/simpleReplace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+
+            example = example.Replace("dishwasher", "galaxy");
+
+            // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+            // Please fix it for me!
+            // Expected ouput: In a galaxy far far away
+            Console.WriteLine(example);
+            Console.ReadLine();
+        }
+    }
+}
