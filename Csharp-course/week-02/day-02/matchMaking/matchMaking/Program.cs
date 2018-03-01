dd25d3a5bfc2ad5939517da3bdf315789033151b
Atis0505 <attilakorom2014@gmail.com>
2018-03-02 00:14:23 +0100

    feat(matchMaking): create method for matchmaking

---
+++ Csharp-course/week-02/day-02/matchMaking/matchMaking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matchMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+
+            // Write a method that joins the two lists by matching one girl with one boy into a new list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            Console.WriteLine(string.Join(" ",MakingMatches(girls, boys)));
+            Console.ReadKey();
+        }
+
+        public static List<string> MakingMatches(List<string> girlList, List<string>boyList)
+        {
+            var matchesList = new List<string>();
+            foreach (string boy in boyList)
+            {
+                matchesList.Add(boy);
+            }
+            for (int i = 0; i < girlList.Count; i++)
+            {
+                matchesList.Insert(i * 2, girlList[i]);
+            }
+            return matchesList;
+        }
+    }
+}
