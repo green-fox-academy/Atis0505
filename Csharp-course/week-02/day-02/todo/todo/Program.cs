e061afc6bc187ff5aa65d998ae780c25b7fb0f6a
Atis0505 <attilakorom2014@gmail.com>
2018-03-01 15:00:04 +0100

    feat(todo): print todo list

---
+++ Csharp-course/week-02/day-02/todo/todo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indentation
+            StringBuilder newString = new StringBuilder();
+            newString.Append("My todo:\n");
+            newString.Append(todoText);
+            newString.Append(" - Donwload games\n   - Diabol");
+            // Expected output:
+            
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            Console.WriteLine(newString);
+            Console.ReadKey();
+        }
+    }
+}
