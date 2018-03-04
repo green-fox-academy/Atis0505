77239ae1f012681f31ccbe558c570278286e14d1
Atis0505 <attilakorom2014@gmail.com>
2018-03-04 21:17:29 +0100

    feat(divideByZero):  create method and catch exeption

---
+++ Csharp-course/week-02/day-02/studentCounter/studentCounter/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new List<Dictionary<string, object>>();
+
+            // Given this list of hashmaps...
+
+            var row0 = new Dictionary<string, object>();
+            row0.Add("name", "Rezso");
+            row0.Add("age", 9.5);
+            row0.Add("candies", 2);
+            map.Add(row0);
+
+            var row1 = new Dictionary<string, object>();
+            row1.Add("name", "Gerzson");
+            row1.Add("age", 10);
+            row1.Add("candies", 1);
+            map.Add(row1);
+
+            var row2 = new Dictionary<string, object>();
+            row2.Add("name", "Aurel");
+            row2.Add("age", 7);
+            row2.Add("candies", 3);
+            map.Add(row2);
+
+            var row3 = new Dictionary<string, object>();
+            row3.Add("name", "Zsombor");
+            row3.Add("age", 12);
+            row3.Add("candies", 5);
+            map.Add(row3);
+
+            var row4 = new Dictionary<string, object>();
+            row4.Add("name", "Olaf");
+            row4.Add("age", 12);
+            row4.Add("candies", 7);
+            map.Add(row4);
+
+            var row5 = new Dictionary<string, object>();
+            row5.Add("name", "Teodor");
+            row5.Add("age", 3);
+            row5.Add("candies", 2);
+            map.Add(row5);
+
+            // Display the following things:
+            //  - Who has got more candies than 4 candies
+            List<object> hasMoreFourCandies = new List<object>();
+
+            foreach (var dictionaryItem in map)
+            {
+                foreach (var student in dictionaryItem)
+                {
+                    if(student.Key == "candies" && Convert.ToInt16(student.Value) > 4)
+                    {
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("Key: {0} , Value: {1}", student.Key, student.Value);
+                }
+            }
+            //  - Sum the age of people who have lass than 5 candies
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp-course/week-03/day-02/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // It should print "fail" if the parameter is 0
+            int number = 2;
+            int number02 = 0;
+            Console.WriteLine("Divide by ten, and handling execption: {0}", DivideByTen(number));
+            Console.WriteLine("Divide by ten, and handling execption: {0}", DivideByTen(number02));
+            Console.ReadKey();
+        }
+
+        public static string DivideByTen(int num)
+        {
+            int result;
+            try
+            {
+                result = 10 / num;
+            }
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
+            
+            return result.ToString();
+        }
+    }
+}
