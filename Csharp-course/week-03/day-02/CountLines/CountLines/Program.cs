dd3050f6b9d53bdd587ccdc0d8bfc228d591c97e
Atis0505 <attilakorom2014@gmail.com>
2018-03-05 05:26:18 +0100

    feat(countLines): count lines from textfile

---
+++ Csharp-course/week-03/day-02/CountLines/CountLines/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+            string txtPath = @"../../textFile.txt";
+            Console.WriteLine("Counted lines number: {0}", CountLines(txtPath));
+            Console.ReadKey();
+        }
+
+        public static int CountLines(string filePath)
+        {
+            int counter = 0;
+            try
+            {
+                using (System.IO.StreamReader file = new System.IO.StreamReader(filePath))
+                {
+                    while (file.ReadLine() != null)
+                    {
+                        counter++;
+                    }
+                    return counter;
+                }
+            }
+            catch (Exception)
+            {
+                return counter;
+            }
+        }
+    }
+}
