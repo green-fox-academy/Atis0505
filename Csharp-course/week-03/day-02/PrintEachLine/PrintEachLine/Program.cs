92a1f16c6ea9fc04c92cb2824f43c31b2985bb66
Atis0505 <attilakorom2014@gmail.com>
2018-03-05 05:16:19 +0100

    feat(printEachLine): print lines from txt, and catch exeption

---
+++ Csharp-course/week-03/day-02/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that opens a file called "my-file.txt", then prints
+            // each of lines form the file.
+            // If the program is unable to read the file (for example it does not exists),
+            // then it should print an error message like: "Unable to read file: my-file.txt"
+            string txtPath = @"../../example_file.txt";
+            string line;
+            try
+            {
+                using(System.IO.StreamReader file = new System.IO.StreamReader(txtPath))
+                {
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
