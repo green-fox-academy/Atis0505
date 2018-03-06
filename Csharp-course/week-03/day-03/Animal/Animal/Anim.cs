b8f959d9f801dfac3b7d9f7c464b06f33241ecfe
Atis0505 <attilakorom2014@gmail.com>
2018-03-06 15:53:59 +0100

    feat(animal): create animal class

---
+++ Csharp-course/week-03/day-03/Animal/Animal/Anim.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Anim
+    {
+        static int hunger;
+        static int thirst;
+
+        public Anim()
+        {
+            hunger = 50;
+            thirst = 50;
+        }
+
+        public void Eat()
+        {
+            hunger--;
+        }
+
+        public void Drink()
+        {
+            thirst--;
+        }
+
+        public void Play()
+        {
+            thirst++;
+            hunger++;
+        }
+
+        public void GetValues()
+        {
+            Console.WriteLine("Hunger point: {0}, Thirst point: {1}", hunger, thirst);
+        }
+    }
+}
---
+++ Csharp-course/week-03/day-03/Animal/Animal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Create an Animal class
+                 Every animal has a hunger value, which is a whole number
+                 Every animal has a thirst value, which is a whole number
+                 when creating a new animal object these values are created with the default 50 value
+                 Every animal can eat() which decreases their hunger by one
+                 Every animal can drink() which decreases their thirst by one
+                 Every animal can play() which increases both by one*/
+
+            var dog = new Anim();
+            for (int i = 0; i < 10; i++)
+            {
+                dog.Play();
+            }
+            dog.GetValues();
+
+            var cat = new Anim();
+            for (int i = 0; i < 4; i++)
+            {
+                cat.Eat();
+            }
+            cat.GetValues();
+
+            Console.ReadKey();
+        }
+    }
+}
