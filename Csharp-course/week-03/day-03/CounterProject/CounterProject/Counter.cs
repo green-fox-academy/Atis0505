2dd68edd841b65ab727d94ac2edf4dd24c7ac0d1
Atis0505 <attilakorom2014@gmail.com>
2018-03-06 16:18:20 +0100

    feat(counter): play with class

---
+++ Csharp-course/week-03/day-03/CounterProject/CounterProject/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject
+{
+    class Counter
+    {
+        int prop;
+
+        public Counter(int num = 0)
+        {
+            this.prop = num;
+        }
+
+        public void Add(int number = 0)
+        {
+            if (number > 0)
+            {
+                this.prop += number;
+            }
+            else
+            {
+                this.prop++;
+            }
+        }
+
+        public string Get()
+        {
+            return this.prop.ToString();
+        }
+
+        public void Reset()
+        {
+            this.prop = 0;
+        }
+    }
+}
---
+++ Csharp-course/week-03/day-03/CounterProject/CounterProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Create Counter class
+                 which has an integer property
+                 when creating it should have a default value 0 or we can specify it when creating
+                 we can Add(number) to this counter another whole number
+                 or we can Add() without parameters just increasing the counter's value by one
+                 and we can Get() the current value as string
+                 also we can Reset() the value to the initial value*/
+            Counter counterA = new Counter();
+            Counter counterB = new Counter(20);
+
+            Console.WriteLine("First without number: {0}",counterA.Get());
+            Console.WriteLine("Second with 20: {0}", counterB.Get());
+
+            counterA.Add(30);
+            counterB.Add();
+
+            Console.WriteLine("First Add method with 30: {0}", counterA.Get());
+            Console.WriteLine("Second without number: {0}", counterB.Get());
+
+            Console.ReadKey();
+        }
+    }
+}
