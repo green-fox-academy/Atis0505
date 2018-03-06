f045053334e7bc862bb2f3819d85173d58146258
Atis0505 <attilakorom2014@gmail.com>
2018-03-06 14:49:07 +0100

    feat(dice): reroll until 6

---
+++ Csharp-course/week-03/day-03/DiceSet/DiceSet/Dice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSet
+{
+    class Dice
+    {
+        // You have a `DiceSet` class which has a list for 6 dices
+        // You can roll all of them with roll()
+        // Check the current rolled numbers with getCurrent()
+        // You can reroll with reroll()
+        // Your task is to roll the dices until all of the dices are 6
+        static Random RandomValue = new Random();
+        static int[] Dices = new int[6];
+
+        public int[] Roll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+            return Dices;
+        }
+
+        public int[] GetCurrent()
+        {
+            return Dices;
+        }
+
+        public int GetCurrent(int i)
+        {
+            return Dices[i];
+        }
+
+        public void Reroll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+        }
+
+        public void Reroll(int k)
+        {
+            Dices[k] = new Random().Next(1, 7);
+        }
+    }
+}
---
+++ Csharp-course/week-03/day-03/DiceSet/DiceSet/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // You have a `DiceSet` class which has a list for 6 dices
+            // You can roll all of them with roll()
+            // Check the current rolled numbers with getCurrent()
+            // You can reroll with reroll()
+            // Your task is to roll the dices until all of the dices are 6
+            var RandomValue = new Random();
+            Dice myDice = new Dice();
+            myDice.Roll();
+            for (int i = 0; i < 6; i++)
+            {
+                while(myDice.GetCurrent()[i] != 6)
+                {
+                    myDice.Reroll(i);
+                }
+            }
+
+            foreach (int item in myDice.GetCurrent())
+            {
+                Console.WriteLine(item);
+            }
+            /*.WriteLine(myDice.GetCurrent());
+            Console.WriteLine(myDice.GetCurrent(5));
+            myDice.Reroll();
+            Console.WriteLine(myDice.GetCurrent());
+            myDice.Reroll(4);
+            myDice.GetCurrent();*/
+
+            Console.ReadKey();
+        }
+    }
+}
