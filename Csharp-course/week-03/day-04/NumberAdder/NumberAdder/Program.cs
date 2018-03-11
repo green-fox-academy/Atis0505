e644727f877627f9b814e9284510489972b908f7
Atis0505 <attilakorom2014@gmail.com>
2018-03-11 14:39:37 +0100

    feat(numberAdder): create method with recursion

---
+++ Csharp-course/week-03/day-04/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        public static int result = 0;
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+            NumberAdder(5);
+            Console.Read();
+        }
+
+        public static void NumberAdder(int number)
+        {
+            
+            if (number == 1)
+            {
+                Console.WriteLine("Number {0}", number);
+                result += 1;
+                Console.WriteLine("Result: {0}", result);
+            }
+            else
+            {
+                result += number;
+                Console.WriteLine("Number {0}", number);
+                NumberAdder(number - 1);
+            }
+        }
+    }
+}
