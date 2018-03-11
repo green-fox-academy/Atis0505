3a52be06961477424a239cf1e047710c7f8cd1cc
Atis0505 <attilakorom2014@gmail.com>
2018-03-11 14:27:42 +0100

    feat(counter): create counter method with recursion

---
+++ Csharp-course/week-03/day-04/Counter/Counter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            Counter(10);
+            Console.ReadKey();
+        }
+
+        public static void Counter(int number)
+        {
+            if(number == 0)
+            {
+                Console.WriteLine("Number: {0}", number);
+            }
+            else
+            {
+                Console.WriteLine("Number: {0}", number);
+                Counter(number - 1);
+            }
+        }
+        
+    }
+}
