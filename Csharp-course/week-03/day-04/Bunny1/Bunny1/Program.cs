3f01205d324d5a5113e97fc7c979587c8aeabf3e
Atis0505 <attilakorom2014@gmail.com>
2018-03-13 17:34:03 +0100

    feat(bunny1): cuont erans of bunnies with recursion

---
+++ Csharp-course/week-03/day-04/Bunny1/Bunny1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny1
+{
+    class Program
+        {
+
+        public static int numberOfBunny = 0;
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+            Console.WriteLine("Number of bunnies: {0}", CountBunny(21));
+            Console.WriteLine("Number of bunnies: {0}", CountBunny(22));
+            Console.ReadKey();
+        }
+
+        public static int CountBunny(int earnNumber)
+        {
+            if (earnNumber % 2 != 0)
+            {
+                Console.WriteLine("The number of bunny's earn should be even number!!!");
+                numberOfBunny = -1;
+            }
+            else if(earnNumber - 2 == 0 )
+            {
+                numberOfBunny += 1;
+            }
+            else
+            {
+                numberOfBunny += 1;
+                CountBunny(earnNumber - 2);
+            }
+            return numberOfBunny;
+        }
+    }
+}
