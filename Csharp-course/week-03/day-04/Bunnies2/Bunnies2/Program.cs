0d2b4b1a5ff134db3a4fdeedc4e1cf2f7747e5e3
Atis0505 <attilakorom2014@gmail.com>
2018-03-14 12:40:09 +0100

    feat(bunnies2): count odd and even bunny with recursion without extend variable

---
+++ Csharp-course/week-03/day-04/Bunnies2/Bunnies2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnies2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+            // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+            // have 3 ears, because they each have a raised foot. Recursively return the
+            // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+            Console.WriteLine("Earn's number with 6 bunnies: {0}", CountBunniesEarn(6));
+            Console.WriteLine("Earn's number with 5 bunnies: {0}", CountBunniesEarn(5));
+            Console.WriteLine("Earn's number with 4 bunnies: {0}", CountBunniesEarn(4));
+            Console.WriteLine("Earn's number with 3 bunnies: {0}", CountBunniesEarn(3));
+            Console.WriteLine("Earn's number with 2 bunnies: {0}", CountBunniesEarn(2));
+            Console.WriteLine("Earn's number with 0 bunnies: {0}", CountBunniesEarn(0));
+            Console.ReadKey();
+        }
+
+        public static int CountBunniesEarn(int bunniesNumber, int numberOfEarn = 0)
+        {
+            if (bunniesNumber == 0)
+            {
+                numberOfEarn += 0;
+            }
+            else
+            {
+                if (bunniesNumber % 2 == 0)
+                {
+                    numberOfEarn += CountBunniesEarn(bunniesNumber - 1, 3);
+                }
+                else
+                {
+                    numberOfEarn += CountBunniesEarn(bunniesNumber - 1, 2);
+                }
+            }
+            return numberOfEarn;
+        }
+    }
+}
