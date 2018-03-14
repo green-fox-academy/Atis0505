2e43ff76c248d9ca9aedf8fe898a5aa0aca1dfa1
Atis0505 <attilakorom2014@gmail.com>
2018-03-14 13:24:04 +0100

    feat(string01): change x to y with recursion in given string

---
+++ Csharp-course/week-03/day-04/String01/String01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+            Console.WriteLine("Green Fox Foxies string : {0}", StringChanger("Green Fox Foxies"));
+            Console.ReadKey();
+        }
+
+        public static string StringChanger(string givenString, string actualChar = "")
+        {
+            if (givenString == "" || givenString == null)
+            {
+                return actualChar;
+            }
+            else
+            {
+                if (givenString[0] != 'x')
+                {
+                    actualChar += StringChanger(givenString.Substring(1), givenString[0].ToString());
+                }
+                else
+                {
+                    actualChar += StringChanger(givenString.Substring(1), "y");
+                }
+            }
+            return actualChar;
+        }
+    }
+}
