024b946d5cde9dfcf5e0c8f0132d74ee369918f6
Atis0505 <attilakorom2014@gmail.com>
2018-03-16 21:32:39 +0100

    feat(fibonacci): fibonacci with recursion

---
+++ Csharp-course/week-03/day-04/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The fibonacci sequence is a famous bit of mathematics, and it happens to
+            // have a recursive definition. The first two values in the sequence are
+            // 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
+            // previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21
+            // and so on. Define a recursive fibonacci(n) method that returns the nth
+            // fibonacci number, with n=0 representing the start of the sequence.
+            Console.WriteLine("Fibonacci 8th number : {0}", Fibonacci(8));
+            Console.ReadKey();
+        }
+
+        public static int Fibonacci(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }else if( number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Fibonacci(number - 1) + Fibonacci(number - 2);
+            }
+        }
+    }
+}
