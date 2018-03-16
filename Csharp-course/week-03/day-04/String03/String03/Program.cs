6aa667669f5e194a7b8b7b3100d5de222b707f78
Atis0505 <attilakorom2014@gmail.com>
2018-03-16 21:24:36 +0100

    feat(string03): separated string by * char with recursion

---
+++ Csharp-course/week-03/day-04/String03/String03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+            Console.WriteLine("Adjancent chars separated by *: {0}", StringChanger("Green fox academy"));
+            Console.ReadKey();
+        }
+
+        public static string StringChanger(string givenString, string actualChar = "")
+        {
+            if (givenString == "" || givenString == null)
+            {
+                return actualChar;
+            }
+            else
+            {
+                actualChar += StringChanger(givenString.Substring(1), givenString[0].ToString()+"*");
+            }
+            return actualChar;
+        }
+    }
+}
