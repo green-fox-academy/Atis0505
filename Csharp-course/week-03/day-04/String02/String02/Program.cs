6f36329c840563c512b7186407e44bd9560564af
Atis0505 <attilakorom2014@gmail.com>
2018-03-16 21:20:36 +0100

    feat(string02): remove all x char from string with recursion

---
+++ Csharp-course/week-03/day-04/String02/String02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+            Console.WriteLine("New string from 'Foxies at Green Fox Academy': {0}",StringChanger("Foxies at Green Fox Academy"));
+            Console.ReadKey();
+        }
+
+        public static string StringChanger(string givenString, string actualChar = "")
+        {
+            if (givenString == "" || givenString == null)
+            {
+                return actualChar;
+            }
+            else
+            {
+                if (givenString[0] != 'x')
+                {
+                    actualChar += StringChanger(givenString.Substring(1), givenString[0].ToString());
+                }
+                else
+                {
+                    actualChar += StringChanger(givenString.Substring(1), "");
+                }
+            }
+            return actualChar;
+        }
+    }
+}
